c711a3a159fcf7d839c502828d8b8d305f672fa2
Jacob Aronoff <jacobaronoff45@gmail.com>
2018-01-13 20:08:47 -0500

    more changes

---
+++ make_post.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DIR="src/pages/$1"
+now="$(date +'%Y-%m-%dT%T')"
+
+echo "-----------------------"
+echo "Making new post called:"
+echo "$DIR"
+echo "-----------------------"
+mkdir $DIR
+touch $DIR/index.md
+echo "---">>$DIR/index.md
+echo "title: REPLACE">>$DIR/index.md
+echo "date: "$now>>$DIR/index.md
+echo "---">>$DIR/index.md
+echo "POST CREATED"
